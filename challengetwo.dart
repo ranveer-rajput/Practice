781168491e5ea260181f74e782882aaa05198a69
ranveer-rajput <106472903+ranveer-rajput@users.noreply.github.com>
2023-07-29 16:11:33 +0530

    format

--- challengetwo.dart
+++ challengetwo.dart
@@ -1,14 +1,10 @@
-void main(){
-
-  List list =[9,7,5,10,12];
-   int max=list[0];
-         for(var i=0;i<list.length;i++){
-          if(max<list[i]){
-            max=list[i];
-          }
-        
-         }
-         print(max);
-         
-
+void main() {
+  List list = [9, 7, 5, 10, 12];
+  int max = list[0];
+  for (var i = 0; i < list.length; i++) {
+    if (max < list[i]) {
+      max = list[i];
+    }
+  }
+  print(max);
 }
