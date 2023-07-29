15b75a4e653b0de55386be995131bf61a7bfa017
ranveer-rajput <106472903+ranveer-rajput@users.noreply.github.com>
2023-07-29 16:07:36 +0530

    find larger number in list ->[dart]

---
+++ challengetwo.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  List list =[9,7,5,10,12];
+   int max=list[0];
+         for(var i=0;i<list.length;i++){
+          if(max<list[i]){
+            max=list[i];
+          }
+        
+         }
+         print(max);
+         
+
+}
